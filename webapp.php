20823fcaa8921c9c3e56b80f65d77177d77c1037
Weston Gallant <wesgallant97@gmail.com>
2018-05-08 07:12:12 -0400

    started web app php

---
+++ webapp.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title> IS218 To do list app </title>
+</head>
+<body>
+	<div class="heading">
+		<h2> To do list application </h2>
+	</div>
+
+	<form method="POST" action="webapp.php">
+		<input type="text" name="task" class="task_input"> 
+		<button type="submit" class="add_btn" name="submit"> Add Task
+		</button>
+	</form>
+
+	<table>
+		<thead> 
+</body>
+</html> 
