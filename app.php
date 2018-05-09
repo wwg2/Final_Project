19382a8e47371134e7db157d4cd2f2e0b31551dc
wwg2 <wwg2@njit.edu>
2018-05-09 03:42:19 -0400

    linked font to google fonts
    also added meta tags

---
+++ app.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>To do</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    
+    <meta name="viewport" content="width=device-width, initial-scale= 1.0">
+  </head>
+  <body>
+  
+  </body>
+</html> 
