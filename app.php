8552ba224830899aa20532f0ead0509217899dc7
wwg2 <wwg2@njit.edu>
2018-05-09 04:20:42 -0400

    Update app.php

--- app.php
+++ app.php
@@ -6,7 +6,7 @@
     
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="main.css">
     
     <meta name="viewport" content="width=device-width, initial-scale= 1.0">
   </head>
