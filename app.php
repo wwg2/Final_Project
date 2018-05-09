69d4c0312fa25488897675966166ccc51e7cb26d
wwg2 <wwg2@njit.edu>
2018-05-09 03:53:29 -0400

    Added input types
    Added placeholder text for input

--- app.php
+++ app.php
@@ -11,6 +11,16 @@
     <meta name="viewport" content="width=device-width, initial-scale= 1.0">
   </head>
   <body>
-  
+    <div class="list">
+      <h1 class="header"> To Do List </h1>
+       <ul>
+         <li><span class="item">Pick up food</span></li> 
+         <li><span class="item done">Learn CSS</span></li> 
+       </ul>
+        <form class="item-add" action="add.php" method="post">
+            <input type="text" name="name" placeholder="Type a new task here" class="input" autocomplete="off" required>
+            <input type="submit" value="add" class="submit">
+        </form> 
+    </div>
   </body>
 </html> 
