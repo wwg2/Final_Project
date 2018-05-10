e170632d0a403445360fb887b2afc4bb1e4e22da
wwg2 <wwg2@njit.edu>
2018-05-10 17:13:38 -0400

    public static functions

--- models/todo.php
+++ models/todo.php
@@ -29,3 +29,63 @@ class Todo {
     public function getDueDate() {
         return $this->createdDate;
     }
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+    public function setCompleted($isCompleted) {
+        $this->isCompleted = $isCompleted;
+        return $this;
+    }
+    public function setUserId($userId) {
+        $this->userId = $userId;
+        return $this;
+    }
+    public function setUserEmail($userEmail) {
+        $this->userEmail = $userEmail;
+        return $this;
+    }
+    public function setCreatedDate($createdDate) {
+        $this->createdDate = $createdDate;
+        return $this;
+    }
+    public function setDueDate($dueDate) {
+        $this->dueDate = $dueDate;
+        return $this;
+    }
+    public function create() {
+        $db = new Database();
+        $query = "INSERT INTO todos (owneremail, ownerid, createddate, duedate, message, isdone) VALUES ('$this->userEmail', $this->userId, NOW(), '$this->dueDate', '$this->title', $this->isCompleted)";
+        $result = $db->query($query);
+        return ['success' => true, 'msg' => 'Your todo has been saved successfully.'];
+    }
+    public static function edit($id, $title, $due_date) {
+        $db = new Database();
+        $query = "UPDATE todos SET message = '$title', duedate = '$due_date' WHERE id = $id";
+        $db->query($query);
+        return ['success' => true, 'msg' => 'Your todo has been edited successfully'];
+    }
+    public static function getTodosByUserId($id) {
+        $db = new Database();
+        $query = "SELECT * FROM todos WHERE ownerid = '$id' ORDER BY duedate";
+        $todos = $db->query($query);
+        return $todos;
+    }
+    public static function deleteTodoById($id) {
+        $db = new Database();
+        $query = "DELETE FROM todos WHERE id = $id";
+        $db->query($query);
+        return ['success' => true, 'msg' => 'Todo has been deleted successfully'];
+    }
+    
+    public static function toggleCompletedById($id) {
+        $db = new Database();
+        $query = "UPDATE todos SET isdone = 1-isdone WHERE id = $id";
+        $db->query($query);
+        return ['success' => true, 'msg' => 'Your todo completion status has been updated'];
+    }
+}
