5794d282a57a30258315f7eee255496889a31164
wwg2 <wwg2@njit.edu>
2018-05-10 17:49:46 -0400

    created home page

---
+++ views/home.php
@@ -0,0 +1,11 @@
+<?php include 'header.php';
+ ?>
+
+<div class="jumbotron text-center">
+  <h1 class="display-3"> WHATS POPPIN? </h1>
+  <p>
+    <a class="btn btn-success btn-lg" href="./login" role="button">Login</a>
+    <a class="btn btn-primary btn-lg" href="./register" role="button">Register</a>
+  </p>
+</div>
+<?php include 'footer.php'; ?>
