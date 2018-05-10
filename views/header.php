e02405dfdb9eb35b1c207b6ca3a62f880a086942
wwg2 <wwg2@njit.edu>
2018-05-10 18:20:12 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -11,7 +11,7 @@
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-    <link href="https://web.njit.edu/~snh7/is218/todo/assets/css/main.css" rel="stylesheet">
+    <link href="https://web.njit.edu/~snh7/Final_Project/assets/style.css" rel="stylesheet">
     <title>Todo App</title>
   </head>
 
@@ -20,7 +20,7 @@
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 
-      <a class="navbar-brand" href="https://web.njit.edu/~snh7/is218/todo/">My Todo App</a>
+      <a class="navbar-brand" href="https://web.njit.edu/~wwg2/Final_Project/index">HOMEPAGE</a>
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto"></ul>
@@ -28,17 +28,17 @@
          ?>
           <ul class="navbar-nav float-right">
             <li class="nav-item active">
-              <a class="nav-link" href="https://web.njit.edu/~snh7/is218/todo/login">Login</a>
+              <a class="nav-link" href="https://web.njit.edu/~wwg2/Final_Project/login">Login</a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="https://web.njit.edu/~snh7/is218/todo/register">Register</a>
+              <a class="nav-link" href="https://web.njit.edu/~wwg2/Final_Project/register">Register</a>
             </li>
           </ul>
         <?php else: 
         ?>
         <ul class="navbar-nav float-right">
               <li class="nav-item active">
-                <a class="nav-link" href="https://web.njit.edu/~snh7/is218/todo/logout">Logout</a>
+                <a class="nav-link" href="https://web.njit.edu/~wwg2/Final_Project/logout">Logout</a>
               </li>
             </ul>
             
