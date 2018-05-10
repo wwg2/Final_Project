89a6eefddcdfeefc678a3c84c5a53ef29c593aa2
wwg2 <wwg2@njit.edu>
2018-05-10 12:36:17 -0400

    Update webapp.php

--- webapp.php
+++ webapp.php
@@ -12,6 +12,17 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
+if (isset($_POST['submit'])) {
+	$task = $_POST['task'];
+	
+	mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
+	header('location: webapp.php');
+} 
+?>
+
+
+
+
 ?>
 
 <html>
