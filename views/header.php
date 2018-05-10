4f9c91f5305d38d5e5890516f471b4270ea21bd7
wwg2 <wwg2@njit.edu>
2018-05-10 18:52:27 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -11,7 +11,7 @@
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-    <link href="https://web.njit.edu/~wwg2/Final_Project/assets/style.css" rel="stylesheet">
+    <link href="https://web.njit.edu/~wwg2/Final_Project/assets/main.css" rel="stylesheet">
     <title>Todo App</title>
   </head>
 
