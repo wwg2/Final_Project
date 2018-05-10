de19ba9109c92288a0a0cfced05a8c09dbf7bbe6
wwg2 <wwg2@njit.edu>
2018-05-10 17:53:27 -0400

    Added div classes

--- views/register.php
+++ views/register.php
@@ -19,3 +19,30 @@
 
               <label for="first-name">First name</label>
               <input type="text" class="form-control" id="first-name" name="first-name" placeholder="First name">
+             
+               </div>
+            <div class="form-group">
+              <label for="last-name">Last name</label>
+              <input type="text" class="form-control" id="last-name" name="last-name" placeholder="Last name">
+            </div>
+
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+            </div>
+
+            <div class="form-group">
+              <label for="tel">Phone</label>
+              <input type="tel" class="form-control" placeholder="(555)-555-5555" name="phone" id="tel">
+            </div>
+
+            <div class="form-group">
+              <label for="dob">Date of birth</label>
+              <input type="date" class="form-control" placeholder="" name="dob" id="dob">
+            </div>
+
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" placeholder="*******" name="password" id="password">
+            </div>
+
