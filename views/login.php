fa9e60f41685941a590afe0f197db0641b841156
wwg2 <wwg2@njit.edu>
2018-05-10 17:51:39 -0400

    added form group

--- views/login.php
+++ views/login.php
@@ -17,3 +17,22 @@
 
 	            <label for="email">Email</label>
 	            <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email address">
+	
+	
+		</div>
+
+	          <div class="form-group">
+
+	            <label for="last-name">Password</label>
+	            <input type="password" class="form-control" id="password" name="password" placeholder="********">
+
+	          </div>
+
+	          <button type="submit" class="btn btn-primary btn-block">Login</button>
+
+	        </form>
+	    </div>
+	<div>
+</div>
+<?php include 'footer.php'
+ ?>
